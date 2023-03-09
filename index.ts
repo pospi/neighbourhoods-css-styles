7e97bf4900c1eeff1c110f94367abcdc4db9cec9
pospi <pospi@spadgos.com>
2023-03-09 15:06:47 +1000

    add base import

---
+++ index.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+import BaseStyles from './nh-style-base.module.css' assert { type: "css" }
+// @ts-ignore
+import LightStyles from './nh-style-light.module.css' assert { type: "css" }
+// @ts-ignore
+import DarkStyles from './nh-style-dark.module.css' assert { type: "css" }
+
+export {
+  BaseStyles, LightStyles, DarkStyles
+}
