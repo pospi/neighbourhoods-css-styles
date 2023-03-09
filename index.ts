6ad0deadac770c603a0ee2d15ac4b85ccef27585
pospi <pospi@spadgos.com>
2023-03-09 15:22:47 +1000

    disable import as CSSStyleSheet for now since Tauri does not support this

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
-import BaseStyles from './nh-style-base.module.css' assert { type: "css" }
+import BaseStyles from './nh-style-base.module.css' //assert { type: "css" }
 // @ts-ignore
-import LightStyles from './nh-style-light.module.css' assert { type: "css" }
+import LightStyles from './nh-style-light.module.css' //assert { type: "css" }
 // @ts-ignore
-import DarkStyles from './nh-style-dark.module.css' assert { type: "css" }
+import DarkStyles from './nh-style-dark.module.css' //assert { type: "css" }
 
 export {
   BaseStyles, LightStyles, DarkStyles
